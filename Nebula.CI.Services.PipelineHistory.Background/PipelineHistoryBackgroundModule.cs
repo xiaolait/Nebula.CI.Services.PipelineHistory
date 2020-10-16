953bb90c5078261f85c3b46094da4e410f4f40e4
xiaolait <xiaolait@outlook.com>
2020-10-16 20:41:02 +0800

    update k8s config

--- Nebula.CI.Services.PipelineHistory.Background/PipelineHistoryBackgroundModule.cs
+++ Nebula.CI.Services.PipelineHistory.Background/PipelineHistoryBackgroundModule.cs
@@ -14,8 +14,21 @@ namespace Nebula.CI.Services.PipelineHistory
     {
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
+            var configuration = context.Services.GetConfiguration();
+            var k8sServer = configuration["K8sServer"];
+
+            KubernetesClientConfiguration config;
+            if(k8sServer != null && k8sServer != string.Empty)
+            {
+                config = new KubernetesClientConfiguration { Host = k8sServer };
+            }
+            else
+            {
+                config = KubernetesClientConfiguration.BuildDefaultConfig();
+            }
+
             context.Services.AddTransient(typeof(Kubernetes), provider => {
-                var config = new KubernetesClientConfiguration { Host = "http://172.18.67.167:8001/" };
+                //var config = new KubernetesClientConfiguration { Host = "http://172.18.67.167:8001/" };
                 //var config = KubernetesClientConfiguration.BuildDefaultConfig();
                 return new Kubernetes(config);
             });
